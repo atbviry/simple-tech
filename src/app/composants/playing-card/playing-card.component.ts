f9c43f990eca3139cd6aa8b1422e030519e58b78
atbviry <atbviry@gmail.com>
2025-07-18 15:49:48 +0200

    Les outputs et signal models | Angular ép. 5

--- src/app/composants/playing-card/playing-card.component.ts
+++ src/app/composants/playing-card/playing-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, Input, InputSignal } from '@angular/core';
+import { Monster } from '../../models/monster';
 
 @Component({
   selector: 'app-playing-card',
@@ -8,5 +9,53 @@ import { Component } from '@angular/core';
   styleUrl: './playing-card.component.css'
 })
 export class PlayingCardComponent {
+  // @Input()name: string = "My monster 2";
+  // @Input()hp: number = 40;
+  // @Input()figureCaption: string = "N°001 Monster"
+  // @Input()attackName: string = "Geo Impact"
+  // @Input()attackStrength: number = 60;
+  // @Input()attackDescription: string = "This is a long description of a monster capacity.";
+
+  //@Input( )monster:Monster = new Monster(); //à default de monster c'est un monstre inconnu (null)
+
+  //on peux faire beaucoup des choses avec les Input
+  // @Input({
+  //   required: true //pour dire que mon input est obligatoire mais ce n'est pas mon but du coup je l'enlève
+  // }
+  // )monster:Monster = new Monster();
+
+  // @Input({
+  //   alias: 'my-monster'
+  // }
+  // ) monster: Monster = new Monster();
+
+  // @Input({
+  //   alias: 'my-monster',
+  //   transform: (value: Monster) =>{
+  //     value.hp = value.hp/2;
+  //     return value;
+  //   },
+  // }
+  // ) monster: Monster = new Monster();
+
+  //utilisation signal input
+  //monster: InputSignal<Monster> = input(new Monster());
+
+  /* monster: InputSignal<Monster> = input(new Monster(), {
+    alias: "my-monster",
+    transform: (value: Monster) => {
+      value.hp = value.hp / 2;
+      return value;
+    }
+  }); */
+  //ou:
+
+  monster: InputSignal<Monster> = input.required({
+    alias: "my-monster",
+    transform: (value: Monster) => {
+      value.hp = value.hp / 2;
+      return value;
+    }
+  });
 
 }
---
+++ src/app/models/monster.ts
@@ -0,0 +1,9 @@
+export class Monster {
+    name: string = "My monster 2";
+    hp: number = 40;
+    figureCaption: string = "N°001 Monster"
+    attackName: string = "Geo Impact"
+    attackStrength: number = 60;
+    attackDescription: string = "This is a long description of a monster capacity.";
+
+}
