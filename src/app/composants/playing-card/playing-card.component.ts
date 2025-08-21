31ef3c40da9ab4f943c955d54c8b1aa4d0c53ca2
atbviry <atbviry@gmail.com>
2025-08-22 00:31:57 +0200

    Détection de changements avec input classique

--- src/app/composants/playing-card/playing-card.component.ts
+++ src/app/composants/playing-card/playing-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, input, Input, InputSignal } from '@angular/core';
 import { Monster } from '../../models/monster';
+import { MonsterTypeProperties } from '../utils/monster.utils';
 
 @Component({
   selector: 'app-playing-card',
@@ -9,53 +10,23 @@ import { Monster } from '../../models/monster';
   styleUrl: './playing-card.component.css'
 })
 export class PlayingCardComponent {
-  // @Input()name: string = "My monster 2";
-  // @Input()hp: number = 40;
-  // @Input()figureCaption: string = "N°001 Monster"
-  // @Input()attackName: string = "Geo Impact"
-  // @Input()attackStrength: number = 60;
-  // @Input()attackDescription: string = "This is a long description of a monster capacity.";
 
-  //@Input( )monster:Monster = new Monster(); //à default de monster c'est un monstre inconnu (null)
-
-  //on peux faire beaucoup des choses avec les Input
-  // @Input({
-  //   required: true //pour dire que mon input est obligatoire mais ce n'est pas mon but du coup je l'enlève
-  // }
-  // )monster:Monster = new Monster();
-
-  // @Input({
-  //   alias: 'my-monster'
-  // }
-  // ) monster: Monster = new Monster();
-
-  // @Input({
-  //   alias: 'my-monster',
-  //   transform: (value: Monster) =>{
-  //     value.hp = value.hp/2;
-  //     return value;
-  //   },
-  // }
-  // ) monster: Monster = new Monster();
-
-  //utilisation signal input
-  //monster: InputSignal<Monster> = input(new Monster());
-
-  /* monster: InputSignal<Monster> = input(new Monster(), {
+  /* monster: InputSignal<Monster> = input.required({
     alias: "my-monster",
     transform: (value: Monster) => {
       value.hp = value.hp / 2;
       return value;
     }
   }); */
-  //ou:
 
-  monster: InputSignal<Monster> = input.required({
-    alias: "my-monster",
-    transform: (value: Monster) => {
-      value.hp = value.hp / 2;
-      return value;
-    }
-  });
+  @Input() monster = new Monster();
+  monsterTypeIcon: string = 'assets/img/electric.png'; //l'electric par defaut
+	backgroundColor: string = 'rgba(255, 255, 104, 1)'; //le jaune par defaut
+
+  constructor() {
+    debugger;
+    this.monsterTypeIcon = MonsterTypeProperties[this.monster.type].imagreUrl;
+    this.backgroundColor = MonsterTypeProperties[this.monster.type].color;
+  }
 
 }
