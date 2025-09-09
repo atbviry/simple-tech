5af7c73d884d8879b5d69d0b693bbe541e54617d
atbviry <atbviry@gmail.com>
2025-09-09 15:23:28 +0200

    routes_navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,34 @@ import { MonstersListComponent } from './pages/monsters-list/monsters-list.compo
 import { MonsterComponent } from './pages/monster/monster.component';
 import { NotFindComponent } from './pages/not-find/not-find.component';
 
+// export const routes: Routes = [{
+//         path:'', 
+//         redirectTo: 'home',pathMatch: 'full'
+// },{
+//         path: "home",
+//         component:MonstersListComponent
+// },{
+//         path: "monster", 
+//         component: MonsterComponent
+// },{
+//         path:'monster/:id',
+//         component: MonsterComponent
+// },{
+//         path:'**',
+//         component: NotFindComponent
+// }];
+//regrouper toutes les routes du compsant Monster dans un seul bloc
 export const routes: Routes = [{
         path:'', 
-        redirectTo: 'home',pathMatch: 'full'
-},{
-        path: "home",
-        component:MonstersListComponent
-},{
+        redirectTo: 'home',pathMatch: 'full'},
+        {
+        path: "home",component:MonstersListComponent},
+        {
         path: "monster", 
-        component: MonsterComponent
-},{
-        path:'monster/:id',
-        component: MonsterComponent
-},{
-        path:'**',
-        component: NotFindComponent
-}];
+        children: [
+            {path:'',component: MonsterComponent},
+            {path:':id',component: MonsterComponent}
+        ]},
+        {
+        path:'**',component: NotFindComponent}
+    ];
--- src/app/pages/monster/monster.component.ts
+++ src/app/pages/monster/monster.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-monster',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './monster.component.html',
   styleUrl: './monster.component.css'
 })
-export class MonsterComponent {
+export class MonsterComponent implements OnInit, OnDestroy {
+//intregation de route
+  private route = inject(ActivatedRoute);
+  monsterId = signal<number | undefined>(undefined);
+
+  //integration de routeur
+  private router = inject(Router);
+  
+  //integration de Subscribe
+  routeSubscription: Subscription | null = null;
+
+
+  // ngOnInit(): void {
+  //   const params = this.route.snapshot.params;
+  //   this.monsterId.set(params['id'] ? parseInt(params['id']) : undefined);
+  // }
+
+  //il faut utilser un systeme d'abonnement pour avoir les bonnes info au fil du temp
+  ngOnInit(): void {
+    this.routeSubscription = this.route.params.subscribe(params => {
+      this.monsterId.set(params['id'] ? parseInt(params['id']) : undefined);
+    })
+  }
+  
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+  }
+
+  next(): void {
+    let nextId = this.monsterId() || 0;
+    nextId++;
+    this.router.navigate(['/monster/' + nextId]);
+
+  }
 
 }
